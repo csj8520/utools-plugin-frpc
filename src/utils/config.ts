13e9e29e0d372906e13577b2897bb7ec65032dbe
csj8520 <2372138557@qq.com>
2025-12-10 19:06:50 +0800

    upgrade: deps

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,7 +2,7 @@ import { useStorage } from '@vueuse/core';
 
 export const config = ref<FrpcConfig>({});
 
-export const customConfig = useStorage<CustomConfig>('custom-config', {});
+export const customConfig = useStorage<CustomConfig>('custom-config', {}) as unknown as Ref<CustomConfig>;
 
 export const allName = computed(() => [...(config.value.proxies ?? []), ...(config.value.visitors ?? [])].map(it => it.name));
 export const allStart = computed(() => {
